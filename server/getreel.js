c8d123e3eb0ca7ae82f952b52dbcf4bf835e309a
IceOnFire <antony.mistretta@gmail.com>
2015-10-10 15:53:35 +0200

    Apply jscs to server code
    Atom's jscs-fixer automatically fixed some problems: now all lines have a tab
    indentation and statements are well spaced. Google's preset also converts all
    double quotes into single quotes unless it's pure JSON.

--- server/getreel.js
+++ server/getreel.js
@@ -4,23 +4,22 @@ Applications = new Mongo.Collection('applications');
 // added repoz channel
 
 Meteor.startup(function() {
-    // console.log('Jobs.remove({})');
-    // Jobs.remove({});
-    if(Jobs.find({}).count()==0) {
-        console.log("job count == ", Jobs.find({}).count(), "inserting jobs");
-        Jobs.insert({_id:'0', title:'Select a job position...'});
-        Jobs.insert({_id:'1', title:'Haiti Village Photographer'});
-        Jobs.insert({_id:'2', title:'Rapallo On The Beach'});
-    } else {
-        console.log('server/getreel.js:', 'job count > 0 (', Jobs.find({}).count(), '): no insert needed');
-    }
+  // console.log('Jobs.remove({})');
+  // Jobs.remove({});
+  if (Jobs.find({}).count() == 0) {
+    console.log('job count == ', Jobs.find({}).count(), 'inserting jobs');
+    Jobs.insert({_id: '0', title: 'Select a job position...'});
+    Jobs.insert({_id: '1', title: 'Haiti Village Photographer'});
+    Jobs.insert({_id: '2', title: 'Rapallo On The Beach'});
+  } else {
+    console.log('server/getreel.js:', 'job count > 0 (', Jobs.find({}).count(), '): no insert needed');
+  }
 });
-
 
 Meteor.methods({
   apply: function(args) {
     if (!Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
+      throw new Meteor.Error('not-authorized');
     }
 
     console.log(args);
