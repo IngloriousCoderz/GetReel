a649fe2d7fcfb45848404ca4588434052a4ec184
IceOnFire <antony.mistretta@gmail.com>
2015-10-14 21:30:46 +0200

    Use FilePicker for file upload and video recording
    I was about to integrate CollectionFS along with the existing
    WebRTC for video recording and Modernizr to check if the feature
    is available. Then I found out that FilePicker does everything and
    even more: it also supports uploading from all cloud services and
    storing to any solution. W00t.

--- apply/client/apply.js
+++ apply/client/apply.js
@@ -1,44 +1,87 @@
+loadFilePicker('ASOqF4I2hQ5O6FgWUBsHLz');
+
 Meteor.subscribe('availableJobs');
 
-Session.set('showUpload', true);
+Session.set('application', {});
 
 Template.Apply.helpers({
-  webrtc: Modernizr.getUserMedia,
+  application: function() {
+    return Session.get('application');
+  },
   availableJobs: function() {
     return Jobs.find();
-  },
-  showUpload: function() {
-    return Session.get('showUpload');
   }
 });
 
 Template.Apply.events({
-  'change input[name="video-type"]': function(e) {
-    Session.set('showUpload', e.target.value === 'file');
+  'change input, change select': function(e) {
+    application = Session.get('application');
+    application[e.target.name] = e.target.value;
+    Session.set('application', application);
+  },
+
+  'click #resume': function(e) {
+    filepicker.pick({
+      mimetypes: [
+        'application/msword',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'application/pdf'
+      ],
+      maxSize: 10 * 1024 * 1024,
+      multiple: false
+    }, function(InkBlob) {
+      application = Session.get('application');
+      application.resume = InkBlob.url;
+      Session.set('application', application);
+    });
+  },
+
+  'click #showreel': function(e) {
+    filepicker.pick({
+      mimetypes: [
+        'video/mp4',
+        'video/3gpp',
+        'video/quicktime',
+        'video/x-msvideo',
+        'video/x-ms-wmv'
+      ],
+      maxSize: 10 * 1024 * 1024,
+      multiple: false
+    }, function(InkBlob) {
+      application = Session.get('application');
+      application.videofile = InkBlob.url;
+      Session.set('application', application);
+    });
   },
 
   'submit form': function(e) {
     e.preventDefault();
     var form = e.target;
-    var application = {
-      createdAt: new Date(),
-      applicant: Meteor.userId(),
-      firstname: form.firstname.value,
-      lastname: form.lastname.value,
-      job: form.job.value,
-      resume: form.resume.value,
-      videofile: form.videofile.value,
-      videolink: form.videolink.value
-    };
+
+    application = Session.get('application');
+    application.createdAt = new Date();
+    application.applicant = Meteor.userId();
 
     /* TODO: form validation */
-    /*for (name in args) {
-      var arg = args[name];
+    /*for (name in application) {
+      var arg = application[name];
       if (arg === undefined || arg === null || arg === '') {
         alert('Please fill in all form values');
         return;
       }
     };*/
+
+    /*if (form.resume) {
+      Resumes.insert(form.resume, function(err, fileObj) {
+        application.resume = fileObj._id;
+      });
+    }
+
+    if (form.videofile) {
+      Showreels.insert(form.videofile, function(err, fileObj) {
+        application.videofile = fileObj._id;
+      });
+    }*/
 
     Meteor.call('submit', application, function(error, result) {
       if (error) {
--- collections.js
+++ collections.js
@@ -1,2 +1,37 @@
 Jobs = new Mongo.Collection('jobs');
 Applications = new Mongo.Collection('applications');
+
+/*
+var profilePicStore = new FS.Store.GridFS('profilePics');
+var createThumb = function(fileObj, readStream, writeStream) {
+  gm(readStream, fileObj.name()).resize('10', '10').stream().pipe(writeStream);
+};
+var profileThumbStore = new FS.Store.GridFS('profileThumbs', {
+  transformWrite: createThumb
+});
+var resumeStore = new FS.Store.GridFS('resumes');
+var showreelStore = new FS.Store.GridFS('showreels');
+
+ProfilePics = new FS.Collection('profilePics', {
+  stores: [
+    profilePicStore,
+    //profileThumbStore
+  ],
+  filter: {
+    maxSize: 1048576,
+    allow: {
+      contentTypes: ['image/*'],
+      extensions: ['png']
+    },
+    onInvalid: function(message) {
+      if (Meteor.isClient) {
+        alert(message);
+      } else {
+        console.log(message);
+      }
+    }
+  }
+});
+Resumes = new FS.Collection('resumes', {stores: [resumeStore]});
+Showreels = new FS.Collection('showreels', {stores: [showreelStore]});
+*/
