f1ac3c5cb6543566bcd0765a0c0a1b4c976924bb
atrox3d <atrox3d@gmail.com>
2015-10-25 19:45:36 +0100

    add fake default users add fake applications fix applications publish

--- recruit/server/recruit.js
+++ recruit/server/recruit.js
@@ -10,7 +10,7 @@ Meteor.startup(function() {
     });
   }
 
-  //if (Meteor.users.find({roles:"admin"}).count() === 0) {
+  Meteor.users.remove({roles:'fake'});
   if (Roles.getUsersInRole('admin').count() === 0) {
       console.log("no admin found, creating fake admin");
       var admin = Accounts.createUser({
@@ -23,9 +23,8 @@ Meteor.startup(function() {
               company: 'inglorious coderz',
           },
       });
-      Roles.addUsersToRoles(admin, "admin");
+      Roles.addUsersToRoles(admin, ['admin', 'fake']);
   }
-  //if (Meteor.users.find({roles:"recruiter"}).count() === 0) {
   if (Roles.getUsersInRole('recruiter').count() === 0) {
       console.log("no recruiters found, creating fake recruiter");
       var recruiter = Accounts.createUser({
@@ -37,10 +36,66 @@ Meteor.startup(function() {
               last_name: 'recruiter',
               company: 'inglorious coderz',
           },
-          roles: ["recruiter"]
       });
-      Roles.addUsersToRoles(recruiter, "recruiter");
+      Roles.addUsersToRoles(recruiter, ['recruiter', 'fake']);
   }
+
+/*
+  var maxFakeUsers = 2;
+  for (var i = 0; i < maxFakeUsers; i++) {
+      var fakeUser = {
+          username: 'utente' + i,
+          email: 'utente' + i + '@getreel.com',
+          password: 'password',
+          profile: {
+              first_name: 'nome' + i,
+              last_name: 'cognome' + i,
+              company: 'company' + i,
+          },
+          roles: ["user"]
+      };
+      Accounts.createUser(fakeUser);
+      Roles.addUsersToRoles(fakeUser, "user");
+  }
+*/
+    console.log("regenerating", maxFakeApplications, "fake applications...");
+    Applications.remove({fake:true})
+    var maxFakeApplications = 50;
+    console.log("generating", maxFakeApplications, "fake applications");
+    for (var i = 0; i < maxFakeApplications; i++) {
+        var fakeApplication = {
+            fake : true,
+            firstname : ['Antony', 'Roby', 'Federica'][Math.floor(Math.random() * 3)],
+            lastname : ['Bianchi', 'Rossi', 'Verdi'][Math.floor(Math.random() * 3)],
+            socialSecurityNumber : "ABCD12345" + i,
+            dateOfBirth : new Date(1970+i%30, (i%12)+1, i),
+            permitKind : "kinda",
+            residentialParish : "comune" ,
+            residentialProvince : "prov" ,
+            residentialCap : 10100,
+            phone : "11223344" + i,
+            mobile : "33344455" + i,
+            status : {},
+            createdAt : new Date(),
+            domicilRegion : ['PIEMONTE', 'LAZIO', 'CAMPANIA'][Math.floor(Math.random() * 3)],
+            previousWorkExperience: "NO",
+            previousPhotographerExperience: "YES",
+        };
+        fakeApplication.status.current = ['unassigned', 'assigned'][Math.floor(Math.random() * 2)];
+        if (fakeApplication.status.current === 'assigned') {
+            fakeApplication.status.to = "recruiter";
+        }
+        fakeApplication.email = fakeApplication.firstname + '.' + fakeApplication.lastname + '@getreel.test';
+        var birthdate = fakeApplication.dateOfBirth;
+        var cur = new Date();
+        var diff = cur-birthdate;
+        var age = Math.floor(diff/31536000000);
+        fakeApplication.age = age;
+
+        Applications.insert(fakeApplication);
+    }
+
+    //console.log("application", i, ":", fakeApplication);
 });
 
 Meteor.publish('applications', function () {
@@ -58,10 +113,12 @@ Meteor.publish('applications', function () {
         return Applications.find({
             $or: [
                     {'status.current':"unassigned"},
+                    {
                     $and: [
-                            {'status.current':"assigned"}
+                            {'status.current':"assigned"},
                             {'status.to':user.username}
                           ]
+                    }
                  ]
         });
     }
