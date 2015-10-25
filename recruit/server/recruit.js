93fb96c8fb4b2a430dd7fc753b879e9f6d4da750
atrox3d <atrox3d@gmail.com>
2015-10-25 17:19:16 +0100

    add "applications" publish

--- recruit/server/recruit.js
+++ recruit/server/recruit.js
@@ -44,18 +44,26 @@ Meteor.startup(function() {
 });
 
 Meteor.publish('applications', function () {
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    var user = Meteor.users.find({_id: this.userId});
+
     if (Roles.userIsInRole(this.userId, ['admin'])) {
         return Applications.find();
     }
 
     if (Roles.userIsInRole(this.userId, ['recruiter'])) {
-        return Applications.find();
+        return Applications.find({
+            $or: [
+                    {'status.current':"unassigned"},
+                    $and: [
+                            {'status.current':"assigned"}
+                            {'status.to':user.username}
+                          ]
+                 ]
+        });
     }
-
-
-    // get user
-    // get role
-    // filter data
-    // return
 });
 
