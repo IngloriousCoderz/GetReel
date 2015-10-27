538faaa50b3a691358b2abdcd75cfc376d828176
IceOnFire <antony.mistretta@gmail.com>
2015-10-27 22:25:21 +0100

    Fix code indentation with JSCS

--- recruit/client/filter.js
+++ recruit/client/filter.js
@@ -1,83 +1,86 @@
 Meteor.subscribe('statuses');
 Meteor.subscribe('regions');
 
-Session.setDefault( 'mongo-filter', {});
+Session.setDefault('mongo-filter', {});
 
 Template.filter.helpers({
-	statuses: function() {
-		return Statuses.find();
-	},
+  statuses: function() {
+    return Statuses.find();
+  },
 
-	regions: function() {
-		return Regions.find();
-	},
+  regions: function() {
+    return Regions.find();
+  },
 });
 
 Template.filter.events({
-	'submit #filter': function(e) {
-		e.preventDefault();
-		var criteria = {
-			creationDate : {
-				not : e.target['createdAt-not'].checked,
-				criterion : e.target['createdAt-criterion'].value,
-				at : e.target.createdAt.value,
-			},
-			recruiter : e.target.recruiter.value,
-			firstname : e.target.firstname.value,
-			lastname : e.target.lastname.value,
-			age : e.target.age.value,
-			mobile : e.target.mobile.value,
-			status : e.target.status.value,
-			region : e.target.region.value,
-		};
-		console.log("criteria:", JSON.stringify(criteria));
+  'submit #filter': function(e) {
+    e.preventDefault();
+    var criteria = {
+      creationDate: {
+        not: e.target['createdAt-not'].checked,
+        criterion: e.target['createdAt-criterion'].value,
+        at: e.target.createdAt.value,
+      },
+      recruiter: e.target.recruiter.value,
+      firstname: e.target.firstname.value,
+      lastname: e.target.lastname.value,
+      age: e.target.age.value,
+      mobile: e.target.mobile.value,
+      status: e.target.status.value,
+      region: e.target.region.value,
+    };
+    console.log('criteria:', JSON.stringify(criteria));
 
-		var mongo = {};
-		var cd = criteria.creationDate;
-		if (cd.at) {
-			cd.at = new Date(cd.at);
-			mongo.createdAt = {};
+    var mongo = {};
+    var cd = criteria.creationDate;
+    if (cd.at) {
+      cd.at = new Date(cd.at);
+      mongo.createdAt = {};
 
-			var condition = "";
-			switch(cd.criterion) {
-				case "eq":
-					var dayAfter = new Date(cd.at);
-					dayAfter.setDate(cd.at.getDate()+1);
-					condition = { $gte: cd.at, $lt: dayAfter };
-				break;
-				case "gt":
-					condition = {$gt: cd.at};
-					break;
-				case "lt":
-					condition = {$lt: cd.at};
-				break;
-				case "between":
-					// todo
-				break;
-				case "empty":
-					//condition = {""}; // empty?
-				break;
-			}
-			if (cd.not) {
-				mongo.createdAt.$not = condition;
-			} else {
-				mongo.createdAt = condition;
-			}
+      var condition = '';
+      switch (cd.criterion) {
+        case 'eq':
+          var dayAfter = new Date(cd.at);
+          dayAfter.setDate(cd.at.getDate() + 1);
+          condition = { $gte: cd.at, $lt: dayAfter };
+        break;
+        case 'gt':
+          condition = {$gt: cd.at};
+        break;
+        case 'lt':
+          condition = {$lt: cd.at};
+        break;
+        case 'between':
 
-		}
+          // todo
+        break;
+        case 'empty':
 
-		for (var criterion in criteria) {
-			if (criteria.hasOwnProperty(criterion)) {
-				if (criteria[criterion] && typeof criteria[criterion] != 'object') {
-					if (criterion === "recruiter") {
-						mongo["status.recruiter"] = criteria[criterion];
-					} else {
-						mongo[criterion] = criteria[criterion];
-					}
-				}
-			}
-		}
-		console.log("mongo query:", JSON.stringify(mongo));
-		Session.set('mongo-filter', mongo);
-	}
+          //condition = {""}; // empty?
+        break;
+      }
+      if (cd.not) {
+        mongo.createdAt.$not = condition;
+      } else {
+        mongo.createdAt = condition;
+      }
+
+    }
+
+    for (var criterion in criteria) {
+      if (criteria.hasOwnProperty(criterion)) {
+        if (criteria[criterion] && typeof criteria[criterion] != 'object') {
+          if (criterion === 'recruiter') {
+            mongo['status.recruiter'] = criteria[criterion];
+          } else {
+            mongo[criterion] = criteria[criterion];
+          }
+        }
+      }
+    }
+
+    console.log('mongo query:', JSON.stringify(mongo));
+    Session.set('mongo-filter', mongo);
+  },
 });
