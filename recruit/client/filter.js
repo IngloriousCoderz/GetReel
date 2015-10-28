28a19bfcd4cd15769bd7c393dbb05837af70a328
IceOnFire <antony.mistretta@gmail.com>
2015-10-28 21:40:08 +0100

    Implement filter elements as own templates

--- recruit/client/filter.js
+++ recruit/client/filter.js
@@ -13,34 +13,7 @@ Template.filter.helpers({
   },
 });
 
-Template.filter.rendered = function() {
-  $(filterForm).find('.filter').change();
-};
-
 Template.filter.events({
-  'change select.filter': function(e) {
-    var criterion = e.target.value;
-    var $values = $(e.target).parent().siblings('.values');
-    switch (criterion) {
-      case 'empty':
-        $values.hide();
-        break;
-      case 'eq':
-      case 'gt':
-      case 'lt':
-        $values.show();
-        $values.children('.value').show();
-        $values.children('.from').hide();
-        $values.children('.to').hide();
-        break;
-      case 'between':
-        $values.show();
-        $values.children('.value').hide();
-        $values.children('.from').show();
-        $values.children('.to').show();
-    }
-  },
-
   'submit #filter': function(e) {
     e.preventDefault();
     var criteria = {
