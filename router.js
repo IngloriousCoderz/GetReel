0fd941854eefecd5841829cc21488677c4bbace3
IceOnFire <antony.mistretta@gmail.com>
2015-10-29 22:56:09 +0100

    Fix container class to pages and template names
    I found out that you can't use reserved words such as 'apply' as templates,
    so we need to stick with 'Apply' in this case or change to 'application'.

--- router.js
+++ router.js
@@ -1,10 +1,10 @@
 Router.configure({
-  layoutTemplate: 'Layout',
+  layoutTemplate: 'layout',
 });
 
 Router.map(function() {
   this.route('/', {name: 'home'});
   this.route('/apply');
   this.route('/apply/success', {name: 'apply.success'});
-  this.route('/recruit', {template: 'PrivateArea' });
+  this.route('/recruit', {template: 'privateArea' });
 });
