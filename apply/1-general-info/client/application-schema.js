81b8168abfba0a88a63693a7ee36bbbd9017a3c1
IceOnFire <antony.mistretta@gmail.com>
2015-10-31 18:25:00 +0100

    Rewrite completely validation
    We dropped meteor-shower, we decided to stick with simple-form-data
    instead. This lead us to use autoform, which does a great job at
    creating inputs with almost no code, and some Bootstrap plugins such
    as bootstrap-datepicker and bootstrap-switch.
    
    NOTE: the Bootstrap-datepicker autoform field does not hold the date,
    We still need to fix this!

---
+++ apply/1-general-info/client/application-schema.js
@@ -0,0 +1,201 @@
+ApplicationSchema = new SimpleSchema({
+  step: {
+    type: Number,
+  },
+  createdAt: {
+    type: Date,
+  },
+
+  firstname: {
+    type: String,
+  },
+  lastname: {
+    type: String,
+  },
+  ssn: {
+    type: String,
+    label: 'Social Security Number',
+  },
+  dateOfBirth: {
+    type: Date,
+  },
+  nationality: {
+    type: String,
+    autoform: {
+      options: {
+        USA: 'USA',
+        UK: 'UK',
+        Italy: 'Italy',
+      },
+    },
+  },
+
+  permit: {
+    type: Boolean,
+    optional: true,
+    defaultValue: true,
+  },
+  permitKind: {
+    type: String,
+    optional: true,
+    custom: function() {
+      if (this.field('permit').value) {
+        return 'required';
+      };
+    },
+  },
+
+  city: {
+    type: String,
+  },
+  province: {
+    type: String,
+    autoform: {
+      options: {
+        Winchestershire: 'Winchestershire',
+        Coltshire: 'Coltshire',
+        Berettashire: 'Berettashire',
+      },
+    },
+  },
+  zip: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  civic: {
+    type: String,
+  },
+  region: {
+    type: String,
+    autoform: {
+      options: {
+        NORTH: 'NORTH',
+        WEST: 'WEST',
+        SOUTH: 'SOUTH',
+      },
+    },
+  },
+  country: {
+    type: String,
+  },
+
+  sameAddress: {
+    type: Boolean,
+    optional: true,
+    defaultValue: true,
+    label: 'Current address same as permanent',
+  },
+
+  currentCity: {
+    type: String,
+    label: 'City',
+  },
+  currentProvince: {
+    type: String,
+    label: 'Province',
+    autoform: {
+      options: {
+        Winchestershire: 'Winchestershire',
+        Coltshire: 'Coltshire',
+        Berettashire: 'Berettashire',
+      },
+    },
+  },
+  currentZip: {
+    type: String,
+    label: 'Zip',
+  },
+  currentAddress: {
+    type: String,
+    label: 'Address',
+  },
+  currentCivic: {
+    type: String,
+    label: 'Civic',
+  },
+  currentRegion: {
+    type: String,
+    label: 'Region',
+    autoform: {
+      options: {
+        NORTH: 'NORTH',
+        WEST: 'WEST',
+        SOUTH: 'SOUTH',
+      },
+    },
+  },
+  currentCountry: {
+    type: String,
+    label: 'Country',
+  },
+  phone: {
+    type: String,
+    label: 'Telephone (home)',
+  },
+  mobile: {
+    type: String,
+    label: 'Mobile phone',
+  },
+  email: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email,
+    label: 'E-mail',
+  },
+  maritalStatus: {
+    type: String,
+  },
+  children: {
+    type: Number,
+  },
+
+  passport: {
+    type: Boolean,
+  },
+  passportNumber: {
+    type: String,
+    optional: true,
+    custom: function() {
+      if (this.field('passport').value) {
+        return 'required';
+      };
+    },
+  },
+  passportValidFrom: {
+    type: Date,
+    optional: true,
+    label: 'from',
+    custom: function() {
+      if (this.field('passport').value) {
+        return 'required';
+      };
+    },
+  },
+  passportValidTo: {
+    type: Date,
+    optional: true,
+    label: 'to',
+    custom: function() {
+      if (this.field('passport').value) {
+        return 'required';
+      };
+    },
+  },
+
+  drivingLicense: {
+    type: Boolean,
+  },
+  carOwner: {
+    type: Boolean,
+  },
+
+  resume: {
+    type: String,
+    // URL
+  },
+  showreel: {
+    type: String,
+    //URL
+  },
+});
--- apply/1-general-info/client/general-info.js
+++ apply/1-general-info/client/general-info.js
@@ -1,145 +1,14 @@
 Template.applyGeneralInfo.onRendered(function() {
   loadFilePicker('ASOqF4I2hQ5O6FgWUBsHLz');
-
-  $('input[type="date"]').each(function(i, elem) {
-    $(elem).datepicker({
-      autoclose: true,
-      dateFormat: 'dd/mm/yyyy',
-    });
-  });
-
-  Shower({
-    id: 'generalInfoForm',
-    fields: {
-      firstname: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      lastname: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      socialSecurityNumber: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      dateOfBirth: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      permitKind: {
-        required: {
-          dependsOn: 'permit',
-          value: true,
-        },
-        requiredMessage: 'This field should not be empty',
-      },
-      residentialParish: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      residentialZip: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      residentialAddress: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      residentialNumber: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      domicileParish: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      domicileZip: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      domicileAddress: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      domicileNumber: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      phone: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      mobile: {
-        required: true,
-        requiredMessage: 'This field should not be empty',
-      },
-      email: {
-        required: true,
-        format: 'email',
-        requiredMessage: 'This field should be a valid email address',
-      },
-      children: {
-        required: true,
-        format: 'integer',
-        requiredMessage: 'This field should not be empty',
-      },
-      passportNumber: {
-        required: {
-          dependsOn: 'passport',
-          value: true,
-        },
-        requiredMessage: 'This field should not be empty',
-      },
-      passportValidFrom: {
-        required: {
-          dependsOn: 'passport',
-          value: true,
-        },
-        requiredMessage: 'This field should not be empty',
-      },
-      passportValidTo: {
-        required: {
-          dependsOn: 'passport',
-          value: true,
-        },
-        requiredMessage: 'This field should not be empty',
-      },
-    },
-
-    onSuccess: function(formData, formHandle) {
-      /*
-      Meteor.call('submit', application, function(error, result) {
-        if (error) {
-          alert('Have you signed in yet? Please do it now');
-        } else {
-          Router.go('/apply/success');
-        }
-      });*/
-      var application = Session.get('application');
-      application.step = 2;
-      Session.set('application', application);
-
-      Shower.Utils.successCallback(formData, formHandle);
-    },
-
-    onFailure: function(erroredFields, formHandle) {
-      for (id in erroredFields) {
-        $('<span>').attr('id', id + '-error').insertAfter($('#' + id));
-      }
-
-      Shower.Utils.failureCallback(erroredFields, $(generalInfoForm));
-    },
-  });
 });
 
 Template.applyGeneralInfo.events({
-  'change :checkbox#permit': function(e) {
+  'switchChange.bootstrapSwitch :checkbox#permit': function(e) {
     var hasPermit = $(e.target).prop('checked');
     $(permitKind).val('').attr('disabled', !hasPermit);
   },
 
-  'change :checkbox#sameAddress': function(e) {
+  'switchChange.bootstrapSwitch :checkbox#sameAddress': function(e) {
     var sameAddress = $(e.target).prop('checked');
     $(currentAddressFieldset).toggleClass('hidden', sameAddress);
   },
@@ -183,8 +52,11 @@ Template.applyGeneralInfo.events({
 
     application = Session.get('application');
     application.createdAt = new Date();
-    application.applicant = Meteor.userId();
 
-    Shower.generalInfoForm.validate(application);
+    check(ApplicationSchema.clean(application), ApplicationSchema);
+
+    application.step = 2;
+    Session.set('application', application);
+    $('#steps li:eq(' + (application.step - 1) + ') a').tab('show');
   },
 });
