eaef7ca3347c832d325b4684c5c091ebb94af547
atrox3d <atrox3d@gmail.com>
2015-11-03 16:03:34 +0100

    Added OutcomeReasons2 Added OutcomeReasons3

---
+++ recruit/server/model/setup/static/outcome.reasons2.js
@@ -0,0 +1,19 @@
+Meteor.startup(function() {
+    OutcomeReasons2.remove({});
+	if (OutcomeReasons2.find().count() === 0) {
+		var reasons = [
+            {id:  1, name: 'COLLOQUIO POSITIVO'},
+            {id:  2, name: 'COLLOQUIO NEGATIVO'},
+            {id:  3, name: 'COLLOQUIO DA FISSARE'},
+            {id:  4, name: 'COLLOQUIO FISSATO'},
+            {id:  4, name: 'COLLOQUIO NON PRESENTATO'},
+            {id:  5, name: 'INTERESSE SUFFIC.'},
+            {id:  6, name: 'BLACK LIST'},
+        ];
+        console.log("adding ", reasons.length, "outcome reasons2...");
+		reasons.forEach(function(reason) {
+			OutcomeReasons2.insert(reason);
+		});
+        console.log("added ", OutcomeReasons2.find().count(), "outcome reasons2.");
+	}
+});
---
+++ recruit/server/model/setup/static/outcome.reasons3.js
@@ -0,0 +1,17 @@
+Meteor.startup(function() {
+    OutcomeReasons3.remove({});
+	if (OutcomeReasons3.find().count() === 0) {
+		var reasons = [
+            {id:  1, name: 'POSITIVO'},
+            {id:  2, name: 'NEGATIVO'},
+            {id:  3, name: 'RISERVA'},
+            {id:  4, name: 'POSITIVO RINUNCIA'},
+            {id:  4, name: 'BLACKLIST'},
+        ];
+        console.log("adding ", reasons.length, "outcome reasons3...");
+		reasons.forEach(function(reason) {
+			OutcomeReasons3.insert(reason);
+		});
+        console.log("added ", OutcomeReasons3.find().count(), "outcome reasons3.");
+	}
+});
