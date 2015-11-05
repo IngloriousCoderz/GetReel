bc04e845483f2363117a67a772ef77faabb07151
palmeranderan <palmeranderan@gmail.com>
2015-11-05 19:49:02 +0100

    Refactoring
    Simple refactor

--- application/3-work/client/work.js
+++ application/3-work/client/work.js
@@ -1,5 +1,4 @@
 Template.workStep.onRendered(function() {
-  loadFilePicker('ASOqF4I2hQ5O6FgWUBsHLz');
   var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
   elems.forEach(function(html) {
     var switchery = new Switchery(html, {size: 'small', color: '#337ab7'});
