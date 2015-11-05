fb716c874d7cc6847f477a8c68ac978ba65e9176
atrox3d <atrox3d@gmail.com>
2015-11-05 12:46:34 +0100

    Add Nations collection

--- backoffice/server/model/setup/career.paths.js
+++ backoffice/server/model/setup/career.paths.js
@@ -12,7 +12,7 @@ Meteor.startup(function() {
         season: 'reference to seasons',
         village: 'import',
         company: 'reference to companies',
-        nation: 'create nations collection',
+        nation: 'TODO: create nations collection',
         roleEvaluation: 'reference to role.evaluations',
         periodFrom: 'import',
         description: 'import',
