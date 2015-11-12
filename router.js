8000bd02ec8f146b7f8fcf839d3d9fed7ba93e11
palmeranderan <palmeranderan@gmail.com>
2015-11-12 16:49:14 +0100

    Refactoring

--- router.js
+++ router.js
@@ -6,5 +6,4 @@ Router.map(function() {
   this.route('/', {name: 'home'});
   this.route('/application/success', {name: 'application.success'});
   this.route('/backoffice');
-  this.route('/application/:step', { name: 'application'});
 });
