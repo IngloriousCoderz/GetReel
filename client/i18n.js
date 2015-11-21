34be1a4182788ec1869496f95b600bd16c7d4ac9
IceOnFire <antony.mistretta@gmail.com>
2015-11-21 13:14:34 +0100

    i18n-ize datepickers

--- client/i18n.js
+++ client/i18n.js
@@ -1,3 +1,8 @@
 Meteor.startup(function() {
   TAPi18n.setLanguage('it');
+  $.fn.datepicker.defaults.language = 'it';
+
+  TAPi18n._afterUILanguageChange = function() {
+    $.fn.datepicker.defaults.language = TAPi18n.getLanguage();
+  };
 });
