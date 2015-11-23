721735a2e2844907263911821878b32b876c5c32
atrox3d <atrox3d@gmail.com>
2015-11-23 17:16:19 +0100

    Setup login based routing

--- router.js
+++ router.js
@@ -2,6 +2,31 @@ Router.configure({
   layoutTemplate: 'layout',
 });
 
+Router.onBeforeAction(function() {
+        var currentUser = Meteor.userId();
+        var currentRoute = Router.current().route.getName();
+
+        if(currentUser) {
+            console.log("GLOBAL::onBeforeAction:logged:", currentRoute);
+            this.next();
+        } else {
+            console.log("GLOBAL::onBeforeAction:NOTlogged:", currentRoute);
+            //this.render("login");
+            this.next();
+        }
+    },
+    {
+        // except: [
+        //             'home',
+        //             'register',
+        //             'login',
+        //             'application'
+        //         ]
+        only: ['backoffice']
+    }
+);
+
+
 Router.route('/', {
     name: 'home'
 });
@@ -11,13 +36,15 @@ Router.route('/application/success', {
 });
 
 Router.route('/backoffice', function() {
-    this.redirect('/backoffice/list');
-});
+        this.redirect('/backoffice/list');
+    }
+);
 
 Router.route('/backoffice/:tab', function() {
-    this.render('backoffice', {
-        data: function() {
-            return {activeTab: this.params.tab};
-        },
-    });
-});
+        this.render('backoffice', {
+            data: function() {
+                return {activeTab: this.params.tab};
+            },
+        });
+    }
+);
